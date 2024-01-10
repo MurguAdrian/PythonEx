48298af2243535513fdb7fd33d8272a78d8e869f
MurguAdrian <murgu.adrian95@gmail.com>
2024-01-10 21:15:57 +0200

    first commit

---
+++ Exercitii Baza.py
@@ -0,0 +1,10 @@
+L=15;
+l=8;
+Aria = L*l;
+print(Aria);
+
+n=int(input("Introdu un nr intreg"));
+if n%2 == 0:
+    print("Numar este par")
+else:
+    print("nr impar")
---
+++ test1.py
@@ -0,0 +1,9 @@
+t = 8
+if t > 8:
+   print("e adevarat,este mare")
+elif t < 1:
+    print("nu e adevarat")
+elif t==3:
+    print("Adevaratt")
+else:
+    print("Nimic mai fals")
